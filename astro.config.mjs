d9b8f4b2e75dbefdf9b7ddd0b8aecea18214db48
Samuele Segrini <97734071+samuelesegrini@users.noreply.github.com>
2026-09-07 16:49:19 +0200

    chore: setup github actions for pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.PUBLIC_SITE_URL ?? 'https://portfolio-placeholder.pages.dev',
+	base: process.env.BASE_URL ?? undefined,
 	integrations: [mdx(), react(), sitemap({ filter: (page) => !page.includes('/lab/') })],
 });
